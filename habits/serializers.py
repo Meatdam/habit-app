45a262660eb9933dfc433e83d2330136341db5ed
Kuzkin Ilya <heartsong_@mail.ru>
2024-07-16 15:02:39 +0500

    downloads habits/serializers.py

---
+++ habits/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+
+from habits.models import Habits
+from habits.validators import HabitsValidatorRelated, ValidatorPrize, ValidatorDuration, ValidatorRelated
+
+
+class HabitSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Habits
+        fields = '__all__'
+        validators = [HabitsValidatorRelated(field='is_good', field2='related'),
+                      ValidatorPrize(field='prize', field2='is_good'), ValidatorDuration(field='duration'),
+                      ValidatorRelated(field='related', field2='prize')]
