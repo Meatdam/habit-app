a263b28e2504e184639e1a8de7b2acf8433b6406
Kuzkin Ilya <heartsong_@mail.ru>
2024-07-16 15:36:21 +0500

    downloads docstring habits/paginators.py

--- habits/paginators.py
+++ habits/paginators.py
@@ -2,6 +2,9 @@ from rest_framework.pagination import PageNumberPagination
 
 
 class Paginator(PageNumberPagination):
+    """
+    Пагинация для списков
+    """
     page_size = 5
     page_size_query_param = 'page_size'
     max_page_size = 100
