b31e660d08cc772b0c4ccc8492b1be3554af3bc0
Kuzkin Ilya <heartsong_@mail.ru>
2024-07-16 15:01:43 +0500

    downloads users/permissions.py file

---
+++ users/permissions.py
@@ -0,0 +1,17 @@
+from rest_framework.permissions import BasePermission
+
+
+class IsSuperuser(BasePermission):
+    """
+    Пермишен для суперюзера
+    """
+    def has_permission(self, request, view):
+        return request.user.is_superuser
+
+
+class IsOwner(BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        if obj.owner == request.user:
+            return True
+        return False
