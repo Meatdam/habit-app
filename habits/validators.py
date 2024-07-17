6f5bf3dec382e89d4deb23ccb6fc5cad93385cb8
Kuzkin Ilya <heartsong_@mail.ru>
2024-07-17 15:05:11 +0500

    add habits/validators.py

--- habits/validators.py
+++ habits/validators.py
@@ -1,3 +1,5 @@
+import datetime
+
 from rest_framework import serializers
 
 
@@ -74,3 +76,40 @@ class ValidatorRelated:
         if related and prize:
             raise serializers.ValidationError(
                 'Связанная привычка не может быть с вознаграждением')
+
+
+class ValidatorTime:
+    """
+    Проверка привычки на коректное ввод даты и времеени на выполнение еженедельной привычки
+    """
+    def __init__(self, field):
+        self.field = field
+
+    def __call__(self, value):
+        time = dict(value).get(self.field)
+        current_date = datetime.datetime.now()
+        result_time = current_date + datetime.timedelta(days=7)
+
+        if time.strftime("%Y-%m-%d %H:%M") > result_time.strftime("%Y-%m-%d %H:%M"):
+            raise serializers.ValidationError(
+                'Привычка может выполняться минимум раз в неделю')
+
+
+class ValidatorTimeDays:
+    """
+    Проверка привычки на коректное ввод даты и времеени на выполнение ежедневной привычки
+    """
+    def __init__(self, field, field2):
+        self.field = field
+        self.field2 = field2
+
+    def __call__(self, value):
+        time = dict(value).get(self.field)
+        is_daily = dict(value).get(self.field2)
+        current_date = datetime.datetime.now()
+        result_time = current_date + datetime.timedelta(days=1)
+
+        if time.strftime("%Y-%m-%d %H:%M") > result_time.strftime("%Y-%m-%d %H:%M") and is_daily is True:
+            raise serializers.ValidationError(
+                'Если вы хотите выполнять привычку не каждый день, поменяйте is_daily на false, или начните с '
+                'завтрашнего дня. Будьте смелее!')
