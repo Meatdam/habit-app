4d89768c34dd823ec6fc92ddfad725f4176b140e
Kuzkin Ilya <heartsong_@mail.ru>
2024-07-17 14:07:47 +0500

    add habits/tasks.py

--- habits/tasks.py
+++ habits/tasks.py
@@ -10,12 +10,15 @@ def send_habit():
     """
     Задача для отправки напоминаний о выполнении полезных привычек в телеграм чатах
     """
+    UTC_HOUR = 5
     habits = Habits.objects.all()
     current_date = datetime.datetime.now()
-
     for habit in habits:
-        if (f"{current_date.time().hour}:{current_date.time().minute}" == f"{habit.time.hour}:{habit.time.minute}"
-                and habit.is_daily is True and habit.mailing_sign is True):
+        if (f"{current_date.year}-{current_date.month}-{current_date.day} {current_date.hour}:{current_date.minute}" ==
+                f"{habit.time.year}-{habit.time.month}-{habit.time.day} {habit.time.hour + UTC_HOUR}:"
+                f"{habit.time.minute}"
+                and habit.is_daily is True and habit.mailing_sign is True and habit.time.day <= current_date.day + 7):
+
             tg_chat = habit.owner.tg_chat_id
             if habit.prize is None:
                 message = (f"Не забудь {habit.action} в {habit.time}' место: '{habit.place}'. И получи приз: "
@@ -24,7 +27,10 @@ def send_habit():
                 message = (f"Не забудь {habit.action} в {habit.time}' место: '{habit.place}'. И получи приз: "
                            f"'{habit.prize}'")
             send_telegram_message(tg_chat, message)
-        elif (f"{current_date.time().hour}:{current_date.time().minute}" == f"{habit.time.hour}:{habit.time.minute}"
+        elif (f"{current_date.year}-{current_date.month}-{current_date.day} {current_date.hour}:"
+              f"{current_date.minute}" == f"{habit.time.year}-{habit.time.month}-{habit.time.day} "
+                                          f"{habit.time.hour + UTC_HOUR}:"
+                f"{habit.time.minute}"
                 and habit.is_daily is False and habit.mailing_sign is True):
             habit.mailing_sign = False
             habit.save()
